d48be6b24eeb4b5ad7bd512fce4ca750b798da89
pikvic <truepikvic@gmail.com>
2023-12-25 18:45:10 +1000

    First version

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
@@ -5,25 +6,73 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from uuid import uuid4
 
-class ChatMessage:
-    message: str
 
+from gigachat import GigaChat
+from gigachat.models import Chat, Messages, MessagesRole
+
+def get_start_payload():
+    return Chat(
+        messages=[
+            Messages(
+                role=MessagesRole.SYSTEM,
+                content="Ты ассистент геолога-исследователя. Тебе необходимо через наводящие вопросы привести геолога к выбору метода обработки его данных, решения его задачи. Цель твоего диалога с ним - выяснить, какие у него данные: текстовые, количественные, картографические, изображения. Начинать диалог всегда надо с приветствия и рассказа о себе. Потом уже геолог будет писать сообщения в ответ на твои наводящие вопросы."
+            )
+        ],
+        temperature=0.7,
+        max_tokens=100,
+    )
+
+def send_start_payload(payload, token):
+    with GigaChat(credentials=token, verify_ssl_certs=False) as giga:
+        response = giga.chat(payload)
+        payload.messages.append(response.choices[0].message)
+    return response.choices[0].message
+
+def send_message(payload, token):
+    with GigaChat(credentials=token, verify_ssl_certs=False) as giga:
+        user_input = input("User: ")
+        payload.messages.append(Messages(role=MessagesRole.USER, content=user_input))
+        response = giga.chat(payload)
+        payload.messages.append(response.choices[0].message)
+        print("Bot: ", response.choices[0].message.content)
+
+
+class ChatMessage(BaseModel):
+    role: str
+    content: str
+
+chats = {}
+
+hello = "Здравствуйте! Я — виртуальный ассистент, который помогает геологам-исследователям в обработке данных и решении задач. Как я могу Вам помочь?"
+
+token = os.environ.get("GIGACHAT_TOKEN", None)
+print(token)
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+async def root(request: Request):
+    chat_id = request.cookies.get("chat_id", None)
+    message = ChatMessage(role="bot", content=hello)
+    response = templates.TemplateResponse("index.html", {"request": request, "messages": [message]})
+    if not chat_id:
+        chat_id = str(uuid4())
+        response.set_cookie(key="chat_id", value=chat_id)
+    chat = get_start_payload()
+    chats[chat_id] = {"chat": chat, "messages": [message]}
+    return response
 
-#async def root(request: Request):
-    #response = templates.TemplateResponse("index.html", {"request": Request})
-    #chat_id = str(uuid4())
-    #response.set_cookie(key="chat_id", value=chat_id)
- #   return {"hello": "fds"}
-
-# @app.post("/chat")
-# async def chat(message: ChatMessage):
-#     return {"message": message}
-    
+@app.post("/chat", response_model=ChatMessage)
+async def chat(request: Request, message: ChatMessage):
+    print(chats)
+    chat_id = request.cookies.get("chat_id", None)
+    payload = chats[chat_id]["chat"]
+    with GigaChat(credentials=token, verify_ssl_certs=False) as giga:
+        payload.messages.append(Messages(role=MessagesRole.USER, content=message.content))
+        response = giga.chat(payload)
+        payload.messages.append(response.choices[0].message)
+        msg = ChatMessage(role="bot", content=response.choices[0].message.content)
+        chats[chat_id]["messages"].append(msg)
+    return msg
